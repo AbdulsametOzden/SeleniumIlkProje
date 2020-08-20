20935b7b3b80bbdfe18f2c3b81eb22395c738752
hamza <hyilmaz.techproed@gmail.com>
2020-08-20 23:33:44 +0300

    deneme

--- src/com/techproed/Day04_GoogleTest.java
+++ src/com/techproed/Day04_GoogleTest.java
@@ -25,6 +25,8 @@ public class Day04_GoogleTest {
         WebElement sonucSayisi = driver.findElement(By.id("result-stats"));
         System.out.println(sonucSayisi.getText());
 
+        System.out.println("selamlar");
+
         WebElement alisverisLinki = driver.findElement(By.partialLinkText("Alışveriş"));
         alisverisLinki.click();
 
